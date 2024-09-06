2e58eb00ae2005f545bdbff1d2181f81408991a6
Hridoy Mnd <hridoymnd10@gmail.com>
2024-09-06 14:30:35 +0600

    task 2 done

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+/*
 // take a function with 4 parameters and multiply the 4 numbers
 function calc(a, b , c ,d){
     let result = a * b * c * d;
     return result;
 }
 console.log(calc(2, 3, 4, 5));
+*/
+// //Take a number if the number is odd multiply the by 2 and if the number is even divide the number by 2 and return the  result
+
+function nmbr(a){
+    if(a % 2 === 0){
+        console.log(" The number is odd ")
+        return a * 2;
+    }
+    else{
+        console.log("The number is even")
+        return a / 2;
+    }
+}
+console.log(nmbr(10));
+console.log(nmbr(15));
+
